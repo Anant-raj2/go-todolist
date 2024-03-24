a012217a4eaffb88b2418d816f702fd0a69a0936
Anant-raj2 <heyanantraj@gmail.com>
2024-03-24 12:11:49 -0500

    cleaned up some code and test commit

--- data/data.go
+++ data/data.go
@@ -4,11 +4,4 @@ type Post struct {
 	ID        string `json:"id"`
 	Title     string `json:"title"`
 	CreatedAt string `json:"created_at"`
-	// ExpiresAt string `json:"expires_at"`
-}
-
-var Posts = []Post{
-	{ID: "1", Title: "Blue Train"},
-	{ID: "2", Title: "Jeru"},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown"},
 }
